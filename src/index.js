993a5b1dcf959b4d90e3cdacb786b51609a8422b
Arthur-art <arthurteixeira.guts@gmail.com>
2021-06-01 14:51:00 -0300

    Inicializando servidor com node-express

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.listen(3000, console.log("Rodando na porta 3000"))
+app.get("/rocketseat", (req, res) => {
+    return res.send("Estudando NodeJs-express na Rocketseat")
+})
