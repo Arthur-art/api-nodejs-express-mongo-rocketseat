a350afc31320701ca7d1d3fd243245a4e59c89ac
Arthur-art <arthurteixeira.guts@gmail.com>
2021-06-17 16:52:35 -0300

    finalizando adicionando tasks na api

--- src/app/controller/projectController.js
+++ src/app/controller/projectController.js
@@ -7,43 +7,52 @@ router.use(authMiddleware)
 const Project = require("../models/project");
 const Task = require("../models/task")
 
-router.get("/", async (req, res)=>{
-    try{
+router.get("/", async (req, res) => {
+    try {
         const projects = await Project.find().populate('user');
-        return res.send({projects}) 
-    }catch(error){
-       return res.status(401).send({error: error});
+        return res.send({ projects })
+    } catch (error) {
+        return res.status(401).send({ error: error });
     }
 });
 
-router.get("/:projectId", async (req, res)=>{
-    try{
+router.get("/:projectId", async (req, res) => {
+    try {
         const projects = await Project.findById(req.params.projectId).populate('user');
-        return res.send({projects}) 
-    }catch(error){ 
-       return res.status(401).send({error: error});
+        return res.send({ projects })
+    } catch (error) {
+        return res.status(401).send({ error: error });
     }
 });
 
-router.post("/", async (req, res)=>{
-    try{
-        const project = await Project.create({...req.body, user: req.userId});
-        return res.send({project});
-    }catch(error){
-       return res.status(401).send({error: 'Erro ao tentar criar um novo projeto'})
+router.post("/", async (req, res) => {
+    try {
+        const { title, description, tasks } = req.body;
+
+        const project = await Project.create({ title, description, user: req.userId });
+
+        await Promise.all(tasks.map(async (task) => {
+            const projectTask = new Task({ ...task, project: project._id });
+            await projectTask.save();
+            project.tasks.push(projectTask)
+        }));
+        await project.save();
+        return res.send({ project });
+    } catch (error) {
+        return res.send(error)
     }
 });
 
-router.put("/", async (req, res)=>{
-    res.send({user: req.userId});
+router.put("/", async (req, res) => {
+    res.send({ user: req.userId });
 });
 
-router.delete("/:projectId", async (req, res)=>{
-    try{
+router.delete("/:projectId", async (req, res) => {
+    try {
         await Project.findByIdAndRemove(req.params.projectId);
-        return res.send({delete: true}) 
-    }catch(error){ 
-       return res.status(401).send({error: error});
+        return res.send({ delete: true })
+    } catch (error) {
+        return res.status(401).send({ error: error });
     }
 });
 
